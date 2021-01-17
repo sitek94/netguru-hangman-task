fe6a9f07714e879936651b80f239a2398dd3218c
sitek94 <msitkowski94@gmail.com>
2021-01-17 23:11:19 +0100

    create and style you-missed component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Folk from 'components/folk';
+import YouMissed from 'components/you-missed';
 import Layout from 'components/layout';
+
+const missedLetters = ['B', 'D', 'E', 'Z', 'P', 'U', 'K', 'L', 'Q', 'W'];
 
 function App() {
   return (
     <Layout>
       <Folk />
+      <YouMissed missedLetters={missedLetters} />
     </Layout>
   );
 }
---
+++ src/components/you-missed.js
@@ -0,0 +1,17 @@
+import PropTypes from 'prop-types';
+import './you-missed.scss';
+
+function YouMissed({ missedLetters = [] }) {
+  return (
+    <div className="you-missed">
+      <div className="title">You missed:</div>
+      <div className="missed-letters">{missedLetters.join(' ')}</div>
+    </div>
+  );
+}
+
+YouMissed.propTypes = {
+  missedLetters: PropTypes.arrayOf(PropTypes.string),
+};
+
+export default YouMissed;
