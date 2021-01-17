71e7ea6c8fdc2835e69bf207431c2160adfdc8d5
sitek94 <msitkowski94@gmail.com>
2021-01-17 22:22:10 +0100

    add the bar; update folk's dimensions

--- src/components/folk.js
+++ src/components/folk.js
@@ -3,23 +3,35 @@ import './folk.scss';
 
 function Folk() {
   return (
-    <div className="folk">
-      <div className="neck" />
-      <img className="head" alt="Folk's head" src={head} />
-      <div className="corpus">
-        <div className="corpus--top" />
-        <div className="corpus--bottom" />
+    <FolkContainer>
+      <div className="folk">
+        <div className="neck" />
+        <img className="head" alt="Folk's head" src={head} />
+        <div className="corpus">
+          <div className="corpus--top" />
+          <div className="corpus--bottom" />
+        </div>
+        <div className="hand hand--left" />
+        <div className="hand hand--right" />
+        <div className="arm arm--left" />
+        <div className="arm arm--right" />
+        <div className="foot foot--left" />
+        <div className="foot foot--right" />
+        <div className="leg leg--left" />
+        <div className="leg leg--right" />
       </div>
-      <div className="hand hand--left" />
-      <div className="hand hand--right" />
-      <div className="arm arm--left" />
-      <div className="arm arm--right" />
-      <div className="foot foot--left" />
-      <div className="foot foot--right" />
-      <div className="leg leg--left" />
-      <div className="leg leg--right" />
+    </FolkContainer>
+  );
+}
+
+function FolkContainer({ children }) {
+  return (
+    <div className="folk-container">
+      <div className="bar bar--horizontal" />
+      <div className="bar bar--vertical" />
+      {children}
     </div>
-  )
+  );
 }
 
 export default Folk;
