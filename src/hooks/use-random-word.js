8e08006ff8ecb200f536eae82ab9f4d8d63fbe9e
sitek94 <msitkowski94@gmail.com>
2021-01-18 17:20:21 +0100

    expose `fetchRandomWord` function

--- src/hooks/use-random-word.js
+++ src/hooks/use-random-word.js
@@ -3,33 +3,35 @@ import * as React from 'react';
 function useRandomWord() {
   const [word, setWord] = React.useState('');
 
-  React.useEffect(() => {
-    const fetchNewWord = async () => {
-      const response = await fetch(
-        `https://wordsapiv1.p.rapidapi.com/words/?random=true`,
-        {
-          method: 'GET',
-          headers: {
-            'x-rapidapi-key': process.env.REACT_APP_RAPIDAPI_KEY,
-            'x-rapidapi-host': process.env.REACT_APP_RAPIDAPI_HOST,
-          },
-        }
-      );
-
-      const { word: fetchedWord } = await response.json();
-
-      // If the word is too long fetch new one
-      if (fetchedWord.length > 11) {
-        fetchNewWord();
-      } else {
-        setWord(fetchedWord.toUpperCase());
+  // Fetches random world from WordsAPI
+  const fetchRandomWord = React.useCallback(async () => {
+    const response = await fetch(
+      `https://wordsapiv1.p.rapidapi.com/words/?random=true`,
+      {
+        method: 'GET',
+        headers: {
+          'x-rapidapi-key': process.env.REACT_APP_RAPIDAPI_KEY,
+          'x-rapidapi-host': process.env.REACT_APP_RAPIDAPI_HOST,
+        },
       }
-    };
+    );
 
-    fetchNewWord();
+    const { word: fetchedWord } = await response.json();
+
+    // If the word is too long fetch new one
+    if (fetchedWord.length > 11) {
+      fetchRandomWord();
+    } else {
+      setWord(fetchedWord.toUpperCase());
+    }
   }, []);
 
-  return word;
+  // Initial fetch
+  React.useEffect(() => {
+    fetchRandomWord();
+  }, [fetchRandomWord]);
+
+  return [word, fetchRandomWord];
 }
 
 export default useRandomWord;
