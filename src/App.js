247c39a8c43baea89e006b054bc33709cc679396
sitek94 <msitkowski94@gmail.com>
2021-01-18 17:21:29 +0100

    add "game over" and "game won" screens

--- src/App.js
+++ src/App.js
@@ -5,10 +5,11 @@ import YouMissed from 'components/you-missed';
 import Letters from 'components/letters';
 import Layout from 'components/layout';
 import useRandomWord from 'hooks/use-random-word';
+import Modal from 'components/modal';
 
 function App() {
   const [usedLetters, setUsedLetters] = React.useState([]);
-  const randomWord = useRandomWord();
+  const [randomWord, fetchRandomWord] = useRandomWord();
 
   // Add/remove key down event listener
   React.useEffect(() => {
@@ -28,11 +29,37 @@ function App() {
     };
   }, [usedLetters]);
 
+  const handleNewWordClick = () => {
+    setUsedLetters([]);
+    fetchRandomWord();
+  };
+
   const missedLetters = usedLetters.filter((l) => !randomWord.includes(l));
   const guessedLetters = usedLetters.filter((l) => randomWord.includes(l));
 
+  // Filter out spaces and dashes
+  const randomWordLetters = randomWord.split('').filter(l => l !== ' ' && l !== '-');
+
+  const isGameOver = missedLetters.length === 11;
+  const isGameWon = guessedLetters.length === randomWordLetters.length;
+
   return (
     <Layout>
+      {isGameOver && (
+        <Modal
+          title="Game over"
+          buttonText="New word"
+          onButtonClick={handleNewWordClick}
+        />
+      )}
+      {isGameWon && (
+        <Modal
+          title="You won!"
+          buttonText="Again"
+          description={`Congratulations, you missed ${missedLetters.length} letters.`}
+          onButtonClick={handleNewWordClick}
+        />
+      )}
       <Folk visiblePartsCount={missedLetters.length} />
       <YouMissed missedLetters={missedLetters} />
       <Letters word={randomWord} guessedLetters={guessedLetters} />
