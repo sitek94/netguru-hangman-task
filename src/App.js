17619d58833751ac2879940377399dbdc2891fe4
sitek94 <msitkowski94@gmail.com>
2021-01-18 12:38:15 +0100

    add basic logic and handle key down

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
+import * as React from 'react';
+
 import Folk from 'components/folk';
 import YouMissed from 'components/you-missed';
 import Letters from 'components/letters';
 import Layout from 'components/layout';
 
-const missedLetters = ['B', 'D', 'E', 'Z', 'P', 'U', 'K', 'L', 'Q', 'W'];
+const word = 'HANGMAN';
 
 function App() {
+  const [usedLetters, setUsedLetters] = React.useState([]);
+
+  React.useEffect(() => {
+    const handleKeyDown = ({ key }) => {
+      // Pressed key is not alphabetical or has been used already
+      if (!/^[a-z]$/i.test(key) || usedLetters.includes(key.toUpperCase())) {
+        return;
+      }
+
+      setUsedLetters(usedLetters.concat(key.toUpperCase()));
+    };
+
+    window.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [usedLetters]);
+
+  const missedLetters = usedLetters.filter((l) => !word.includes(l));
+  const guessedLetters = usedLetters.filter((l) => word.includes(l));
+
   return (
     <Layout>
       <Folk />
       <YouMissed missedLetters={missedLetters} />
-      <Letters />
+      <Letters word={word} guessedLetters={guessedLetters} />
     </Layout>
   );
 }
