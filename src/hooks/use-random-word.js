fd8bb8b21e2110d50993b2cb2e89ad65bd11e2f0
sitek94 <msitkowski94@gmail.com>
2021-01-18 17:54:53 +0100

    handle request status change (pending, resolved, etc.)

--- src/hooks/use-random-word.js
+++ src/hooks/use-random-word.js
@@ -2,27 +2,39 @@ import * as React from 'react';
 
 function useRandomWord() {
   const [word, setWord] = React.useState('');
+  const [status, setStatus] = React.useState('idle');
+  const [error, setError] = React.useState(null);
 
   // Fetches random world from WordsAPI
   const fetchRandomWord = React.useCallback(async () => {
-    const response = await fetch(
-      `https://wordsapiv1.p.rapidapi.com/words/?random=true`,
-      {
-        method: 'GET',
-        headers: {
-          'x-rapidapi-key': process.env.REACT_APP_RAPIDAPI_KEY,
-          'x-rapidapi-host': process.env.REACT_APP_RAPIDAPI_HOST,
-        },
+    setStatus('pending');
+
+    try {
+      const response = await fetch(
+        `https://wordsapiv1.p.rapidapi.com/words/?random=true`,
+        {
+          method: 'GET',
+          headers: {
+            'x-rapidapi-key': process.env.REACT_APP_RAPIDAPI_KEY,
+            'x-rapidapi-host': process.env.REACT_APP_RAPIDAPI_HOST,
+          },
+        }
+      );
+
+      const { word: fetchedWord } = await response.json();
+
+      // If the word is too long fetch new one
+      if (fetchedWord.length > 11) {
+        fetchRandomWord();
+      } else {
+        setWord(fetchedWord.toUpperCase());
+        setStatus('resolved');
       }
-    );
+    } catch (e) {
+      setStatus('rejected');
+      setError(e);
 
-    const { word: fetchedWord } = await response.json();
-
-    // If the word is too long fetch new one
-    if (fetchedWord.length > 11) {
-      fetchRandomWord();
-    } else {
-      setWord(fetchedWord.toUpperCase());
+      console.log(e);
     }
   }, []);
 
@@ -31,7 +43,7 @@ function useRandomWord() {
     fetchRandomWord();
   }, [fetchRandomWord]);
 
-  return [word, fetchRandomWord];
+  return [{ status, randomWord: word, error }, fetchRandomWord];
 }
 
 export default useRandomWord;
