c84ba1ade6dcc9f947c65efd3f84c5bf18c65398
sitek94 <msitkowski94@gmail.com>
2021-01-19 18:42:21 +0100

    write more tests

---
+++ src/hooks/use-random-word.test.js
@@ -0,0 +1,50 @@
+import { useRandomWord } from './use-random-word';
+import { act, renderHook } from '@testing-library/react-hooks';
+
+describe('useRandomWord hook', () => {
+  it('handles state when is fetching a word', async () => {
+    jest.spyOn(global, 'fetch').mockReturnValue(
+      new Promise((resolve) => {
+        resolve({
+          json: () => Promise.resolve({ word: 'sth' }),
+        });
+      })
+    );
+
+    const { result } = renderHook(useRandomWord);
+
+    expect(result.current.status).toBe('pending');
+
+    await act(async () => result.current.fetchRandomWord());
+
+    expect(result.current.status).toBe('resolved');
+  });
+
+  it('handles state when successfully fetched a word', async () => {
+    jest.spyOn(global, 'fetch').mockReturnValue(
+      Promise.resolve({
+        json: () => Promise.resolve({ word: 'hangman' }),
+      })
+    );
+
+    const { result } = renderHook(useRandomWord);
+
+    await act(async () => result.current.fetchRandomWord());
+
+    expect(result.current.status).toBe('resolved');
+  });
+
+  it('handles state when failed to fetch a word', async () => {
+    jest
+      .spyOn(global, 'fetch')
+      .mockReturnValue(
+        Promise.reject({ message: 'Failed to fetch a random word' })
+      );
+
+    const { result } = renderHook(useRandomWord);
+
+    await act(async () => result.current.fetchRandomWord());
+
+    expect(result.current.status).toBe('rejected');
+  });
+});
